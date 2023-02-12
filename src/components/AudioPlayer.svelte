8467f9e2ccd5f7dc1768dae969b3b858b107208f
Rico van Zelst <ricovzelst@pm.me>
2023-02-12 16:25:06 +0100

    small tailwind styling

--- src/components/AudioPlayer.svelte
+++ src/components/AudioPlayer.svelte
@@ -55,25 +55,39 @@
   });
 </script>
 
-<select on:change={handleGenreSelection}>
-  {#each genres as genre}
-    <option value={genre} selected={selectedGenre === genre}>{genre}</option>
-  {/each}
-</select>
+<div class="flex flex-col items-center mt-10">
+  <div class="text-lg font-bold mb-3">Select a genre:</div>
+  <select
+    class="bg-white border border-gray-400 rounded p-2"
+    on:change={handleGenreSelection}
+  >
+    {#each genres as genre}
+      <option value={genre} selected={selectedGenre === genre}>{genre}</option>
+    {/each}
+  </select>
+</div>
 
-<audio src={currentSong} autoplay loop bind:volume />
+<audio src={currentSong} autoplay loop bind:volume class="hidden" />
 
-<p>Music volume:</p>
-<input
-  type="range"
-  min="0"
-  max="1"
-  step="0.01"
-  value={volume}
-  on:input={handleVolumeChange}
-/>
+<div class="flex flex-col items-center mt-10">
+  <div class="text-lg font-bold mb-3">Music volume:</div>
+  <input
+    type="range"
+    min="0"
+    max="1"
+    step="0.01"
+    value={volume}
+    on:input={handleVolumeChange}
+    class="w-64"
+  />
+</div>
 
-<button on:click={handleNextSong}>Next song</button>
+<button
+  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-10 rounded"
+  on:click={handleNextSong}
+>
+  Next song
+</button>
 
 {#each effects as effect}
   <audio
@@ -81,15 +95,19 @@
     loop
     bind:volume={effectVolumes[effect].volume}
     bind:paused={effectVolumes[effect].paused}
+    class="hidden"
   />
 
-  <p>{effect} volume:</p>
-  <input
-    type="range"
-    min="0"
-    max="1"
-    step="0.01"
-    value={effectVolumes[effect].volume}
-    on:input={handleEffectVolumeChange(effect)}
-  />
+  <div class="flex flex-col items-center mt-10">
+    <div class="text-lg font-bold mb-3">{effect} volume:</div>
+    <input
+      type="range"
+      min="0"
+      max="1"
+      step="0.01"
+      value={effectVolumes[effect].volume}
+      on:input={handleEffectVolumeChange(effect)}
+      class="w-64"
+    />
+  </div>
 {/each}
